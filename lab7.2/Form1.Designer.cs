8a7fef8a4c9b883bf0be069d6e99a1badcff7fbf
Kathleen Forgiarini da Silva <82175930+kathleenforgiarini@users.noreply.github.com>
2023-03-28 08:50:48 -0400

    Add files via upload

---
+++ lab7.2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace lab7._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fName = new System.Windows.Forms.TextBox();
+            this.lName = new System.Windows.Forms.TextBox();
+            this.wtext = new System.Windows.Forms.Button();
+            this.rtext = new System.Windows.Forms.Button();
+            this.rbinary = new System.Windows.Forms.Button();
+            this.wbinary = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FName: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LName: ";
+            // 
+            // fName
+            // 
+            this.fName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fName.Location = new System.Drawing.Point(98, 23);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(174, 26);
+            this.fName.TabIndex = 2;
+            // 
+            // lName
+            // 
+            this.lName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lName.Location = new System.Drawing.Point(97, 59);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(174, 26);
+            this.lName.TabIndex = 3;
+            // 
+            // wtext
+            // 
+            this.wtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wtext.Location = new System.Drawing.Point(97, 123);
+            this.wtext.Name = "wtext";
+            this.wtext.Size = new System.Drawing.Size(123, 49);
+            this.wtext.TabIndex = 4;
+            this.wtext.Text = "Write text \r\nfile";
+            this.wtext.UseVisualStyleBackColor = true;
+            this.wtext.Click += new System.EventHandler(this.wtext_Click);
+            // 
+            // rtext
+            // 
+            this.rtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtext.Location = new System.Drawing.Point(227, 123);
+            this.rtext.Name = "rtext";
+            this.rtext.Size = new System.Drawing.Size(123, 49);
+            this.rtext.TabIndex = 5;
+            this.rtext.Text = "Read text \r\nfile";
+            this.rtext.UseVisualStyleBackColor = true;
+            this.rtext.Click += new System.EventHandler(this.rtext_Click);
+            // 
+            // rbinary
+            // 
+            this.rbinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbinary.Location = new System.Drawing.Point(227, 192);
+            this.rbinary.Name = "rbinary";
+            this.rbinary.Size = new System.Drawing.Size(123, 49);
+            this.rbinary.TabIndex = 7;
+            this.rbinary.Text = "Read binary \r\nfile";
+            this.rbinary.UseVisualStyleBackColor = true;
+            this.rbinary.Click += new System.EventHandler(this.rbinary_Click);
+            // 
+            // wbinary
+            // 
+            this.wbinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wbinary.Location = new System.Drawing.Point(98, 192);
+            this.wbinary.Name = "wbinary";
+            this.wbinary.Size = new System.Drawing.Size(123, 49);
+            this.wbinary.TabIndex = 6;
+            this.wbinary.Text = "Write binary \r\nfile";
+            this.wbinary.UseVisualStyleBackColor = true;
+            this.wbinary.Click += new System.EventHandler(this.wbinary_Click);
+            // 
+            // exit
+            // 
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Location = new System.Drawing.Point(395, 201);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(116, 30);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 252);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.rbinary);
+            this.Controls.Add(this.wbinary);
+            this.Controls.Add(this.rtext);
+            this.Controls.Add(this.wtext);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.fName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fName;
+        private System.Windows.Forms.TextBox lName;
+        private System.Windows.Forms.Button wtext;
+        private System.Windows.Forms.Button rtext;
+        private System.Windows.Forms.Button rbinary;
+        private System.Windows.Forms.Button wbinary;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ lab7.2/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+// CODED BY KATHLEEN FORGIARINI DA SILVA
+// 2023-03-27
+// LAB 7.2 - WORKING WITH TEXT AND BINARY FILES
+
+namespace lab7._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        string path = @".\files\Names.txt";
+        private void wtext_Click(object sender, EventArgs e)
+        {
+            if (fName.Text == "")
+            {
+                MessageBox.Show("FName is a required field.");
+            }
+            else if (lName.Text == "")
+            {
+                MessageBox.Show("LName is a required field.");
+            }
+            else
+            {
+                string dir = @".\files\";
+                FileStream fileStream = null;
+
+                try
+                {
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+                    fileStream = new FileStream(path, FileMode.Append, FileAccess.Write);
+                    StreamWriter writer = new StreamWriter(fileStream);
+                    writer.Write(fName.Text + ";" + lName.Text + "\n");
+
+                    writer.Close();
+                    fileStream.Close();
+
+                    MessageBox.Show("Data was written to the file successfully.");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occured, try again. \n" + ex.Message);
+                }
+                finally
+                {
+                    if (fileStream != null) fileStream.Close();
+                }
+            }
+        }
+
+        private void rtext_Click(object sender, EventArgs e)
+        {
+            FileStream fileStream = null;
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fileStream);
+
+                string textToPrint = "";
+                int counter = 0;
+                while (reader.Peek() != -1)
+                {
+                    string line = reader.ReadLine();
+                    string[] data = line.Split(';');
+                    textToPrint += data[0] + " " + data[1] + "\n";
+                    counter++;
+
+                    if (counter == 3)
+                    {
+                        MessageBox.Show(textToPrint);
+                        textToPrint = "";
+                        counter = 0;
+                    }
+                }
+
+                if (counter != 0)
+                {
+                    MessageBox.Show(textToPrint);
+                }
+
+                reader.Close();
+                fileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while reading from the file: \n" + ex.Message);
+            }
+            finally
+            {
+                if (fileStream != null) fileStream.Close();
+            }
+        }
+
+        string pathBinary = @"C:.\files\BNames.dat";
+        private void wbinary_Click(object sender, EventArgs e)
+        {
+            if (fName.Text == "")
+            {
+                MessageBox.Show("FName is a required field.");
+            }
+            else if (lName.Text == "")
+            {
+                MessageBox.Show("LName is a required field.");
+            }
+            else
+            {
+                string dir = @".\files\";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(pathBinary, FileMode.Append, FileAccess.Write);
+                    BinaryWriter binaryOut = new BinaryWriter(fs);
+                    binaryOut.Write(fName.Text.Trim());
+                    binaryOut.Write(lName.Text.Trim());
+                    binaryOut.Close();
+                    fs.Close();
+
+                    MessageBox.Show("Data was written to the file successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occured, try again. \n" + ex.Message);
+                }
+                finally
+                {
+                    if (fs != null) fs.Close();
+                }
+            }
+        }
+
+        private void rbinary_Click(object sender, EventArgs e)
+        {
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(pathBinary, FileMode.OpenOrCreate, FileAccess.Read);
+                BinaryReader binaryIn = new BinaryReader(fs);
+                string textToPrint = "FName \t LName \n";
+                string LName, FName;
+                int counter = 0;
+                while (binaryIn.PeekChar() != -1)
+                {
+                    FName = binaryIn.ReadString();
+                    LName = binaryIn.ReadString();
+                    textToPrint += FName + ",\t" + LName + "\n";
+                    counter++;
+
+                    if (counter == 3)
+                    {
+                        MessageBox.Show(textToPrint);
+                        textToPrint = "";
+                        counter = 0;
+                    }
+                }
+
+                if (counter != 0)
+                {
+                    MessageBox.Show(textToPrint);
+                }
+
+                binaryIn.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while reading from the file: \n" + ex.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
